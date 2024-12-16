ba007c8873d2246870784ad9c2ddff0387253b99
AbdaalKhan01 <164651168+AbdaalKhan01@users.noreply.github.com>
2024-12-16 06:41:16 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+
+import Hero1 from "@/components/hero1";
+import Hero2 from "@/components/hero2";
+import Hero3 from "@/components/hero3";
+import Blogs from "@/components/blogs";
+import Insta from "@/components/insta";
+import Head from "@/components/head";
+
+
+export default function Home() {
+  return (
+   <div className="">
+    <Head/>
+<Hero1/>
+<Hero2/>
+<Hero3/>
+<Blogs/>
+<Insta/>
+   </div>
+  );
+}
